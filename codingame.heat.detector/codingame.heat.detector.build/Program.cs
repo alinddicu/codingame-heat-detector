f6eadd806a9c13b94248c587cc4991ff75f342f5
Alin Dicu <alinddicu@yahoo.com>
2017-03-22 22:00:53 +0100

    added project codingame.heat.detector.build

---
+++ codingame.heat.detector/codingame.heat.detector.build/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace codingame.heat.detector.build
+{
+	using System.IO;
+	using System.Linq;
+	using System.Text;
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			const string folderPath = "../../../codingame.heat.detector/";
+			var contents = new StringBuilder();
+			AppendFilesContents(folderPath, contents);
+
+			File.WriteAllText("../../codingame.heat.detector.txt", contents.ToString());
+		}
+
+		private static void AppendFilesContents(string root, StringBuilder contents)
+		{
+			foreach (var file in Directory.EnumerateFiles(root, "*.cs").Where(f => !f.EndsWith("AssemblyInfo.cs")))
+			{
+				contents.AppendLine(File.ReadAllText(file));
+				contents.AppendLine();
+			}
+
+			foreach (var d in Directory.GetDirectories(root))
+			{
+				AppendFilesContents(d, contents);
+			}
+		}
+	}
+}
