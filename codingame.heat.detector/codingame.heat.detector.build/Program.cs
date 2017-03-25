a31a5bb94e3958c9044923b0d065432077b50531
Alin Dicu <alinddicu@yahoo.com>
2017-03-25 10:40:38 +0100

    added and used reference to codingame.common.build.submit.file

--- codingame.heat.detector/codingame.heat.detector.build/Program.cs
+++ codingame.heat.detector/codingame.heat.detector.build/Program.cs
@@ -1,32 +1,14 @@
 ﻿namespace codingame.heat.detector.build
 {
-	using System.IO;
-	using System.Linq;
-	using System.Text;
+	using common.build.submit.file;
 
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			const string folderPath = "../../../codingame.heat.detector/";
-			var contents = new StringBuilder();
-			AppendFilesContents(folderPath, contents);
-
-			File.WriteAllText("../../codingame.heat.detector.txt", contents.ToString());
-		}
-
-		private static void AppendFilesContents(string root, StringBuilder contents)
-		{
-			foreach (var file in Directory.EnumerateFiles(root, "*.cs").Where(f => !f.EndsWith("AssemblyInfo.cs")))
-			{
-				contents.AppendLine(File.ReadAllText(file));
-				contents.AppendLine();
-			}
-
-			foreach (var d in Directory.GetDirectories(root))
-			{
-				AppendFilesContents(d, contents);
-			}
+			const string sourceFolderPath = "../../../codingame.heat.detector/";
+			const string targetFilePath = "../../codingame.heat.detector.txt";
+			new BuildSubmitFile().Build(sourceFolderPath, targetFilePath);
 		}
 	}
 }
