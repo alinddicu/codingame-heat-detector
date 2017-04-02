b9b4776f75c539fe44500ffc0dc9a480f5194ca5
Alin Dicu <alinddicu@yahoo.com>
2017-04-02 21:15:08 +0200

    cleaned window

--- codingame.heat.detector/codingame.heat.detector/Window.cs
+++ codingame.heat.detector/codingame.heat.detector/Window.cs
@@ -23,31 +23,5 @@
 		{
 			return X + " " + Y;
 		}
-
-		public override bool Equals(object obj)
-		{
-			if (ReferenceEquals(null, obj))
-			{
-				return false;
-			}
-
-			if (ReferenceEquals(this, obj))
-			{
-				return true;
-			}
-
-			var other = obj as Window;
-			return other != null && Equals(other);
-		}
-
-		private bool Equals(Window other)
-		{
-			return X == other.X && Y == other.Y;
-		}
-
-		public override int GetHashCode()
-		{
-			return ToString().GetHashCode();
-		}
 	}
 }
